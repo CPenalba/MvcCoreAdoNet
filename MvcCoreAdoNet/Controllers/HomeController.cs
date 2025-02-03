0787636044fe18628b22ce1dbd8e3b136917f79b
CPenalba <carolinapenalbacorpa@gmail.com>
2025-02-03 09:58:42 +0100

    Add project files.

---
+++ MvcCoreAdoNet/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAdoNet.Models;
+
+namespace MvcCoreAdoNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EjemploSection()
+        {
+            return View();
+        }
+
+        public IActionResult VistaPersona()
+        {
+            Persona p = new Persona();
+            p.Nombre = "Alumno Core";
+            p.Email = "alumnocore@gmail.com";
+            p.Edad = 25;
+            return View(p);
+        }
+    }
+}
